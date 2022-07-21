bea79ef44adc879f3ba6be7995d24c745172b126
manasseh liassidji <manassehl9@gmail.com>
2022-07-21 16:07:54 +0100

    The richest customer is the customer that has the maximum wealth

---
+++ Array/RichestCustomer/index.js
@@ -0,0 +1,10 @@
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer 
+// has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+// A customer's wealth is the amount of money they have in all their bank accounts.
+//  The richest customer is the customer that has the maximum wealth.
+
+
+var maximumWealth = function (accounts) {
+  return Math.max(...accounts.map((e) => e.reduce((x, y) => x + y)));
+};
